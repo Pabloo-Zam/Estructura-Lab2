60b5da89f7cd6250bec82e5afa312c5e17c8c3d2
pabloo-zam <PFZAMORA@Correo.url.edu.gt>
2023-03-16 19:28:25 -0600

    Lectura de json

---
+++ Laboratorio1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Laboratorio1
+{
+    internal class Program
+    {
+        public class InputLab
+        {
+            public Dictionary<string, bool>[] input1 { get; set; }
+            public string[] input2 { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            foreach (string line in System.IO.File.ReadLines(@"D:\Desktop\1er ciclo 2023\Estructura Lab\Estructura-Lab2\input_lab_2_example.jsonl"))
+            {
+                System.Console.WriteLine(line);
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                    IncludeFields = true,
+                };
+               
+                System.Console.ReadLine();
+            }
+        }
+    }
+}
